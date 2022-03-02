eb7f534a58035b6a4f4fd29d6aa02acc65b0fc40
yang <315305689@qq.com>
2022-03-02 21:12:05 +0800

    7.2.1结束,但是需要解决一个Bug:post/2.html/ 无法匹配,为什么多了一个 /

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -2,13 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post, Tag, Category
+
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):  # 这里传进来的参数就是url中的正则表达式
-    content = 'post_list category_id={category_id},tag_id={tag_id}'.format(category_id=category_id,
-                                                                           tag_id=tag_id)  # 这里一定要给出值传参,
-    return render(request, 'blog/list.html', context={'name': 'post_list'})  # 这里的request封装了HTTP的请求
+    tag = None
+    category = None
+
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                category = None
+            else:
+                post_list = post_list.filter(category_id=category_id)
+
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+
+    # content = 'post_list category_id={category_id},tag_id={tag_id}'.format(category_id=category_id,
+    #                                                                        tag_id=tag_id)  # 这里一定要给出值传参,
+    return render(request, 'blog/list.html', context=context)  # 这里的request封装了HTTP的请求
 
 
-def post_detail(request, post_id):
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+def post_detail(request, post_id=None):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
